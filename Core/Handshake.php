41c95c679bff99fe3c86baa8db2b53c8fabb6682
Denis Priebe <denis.priebe@irishtitan.com>
2017-03-27 15:08:59 -0500

    Updated Handshake configuration.
    Handshake will now try and use Magento’s database configuration.

--- Core/Handshake.php
+++ Core/Handshake.php
@@ -16,9 +16,9 @@ class Handshake
         'driver' => 'mysql',
         'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'testapp_magento',
+        'database' => 'databasename',
         'username' => 'root',
-        'password' => 'mysql',
+        'password' => 'password',
         'unix_socket' => '',
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
@@ -34,7 +34,30 @@ class Handshake
     public static function start()
     {
         $handshake = new static;
+        $handshake->setConfig();
         $handshake->bootEloquent();
+    }
+
+    /**
+     * Get Magento's configuration.
+     *
+     * @return mixed
+     */
+    private function config()
+    {
+        return include 'app/etc/env.php';
+    }
+
+    /**
+     * Load Magento's configuration into Handshake.
+     *
+     */
+    private function setConfig()
+    {
+        $config = $this->config();
+        $this->config['database'] = $config['db']['connection']['default']['dbname'];
+        $this->config['username'] = $config['db']['connection']['default']['username'];
+        $this->config['password'] = $config['db']['connection']['default']['password'];
     }
 
     /**
